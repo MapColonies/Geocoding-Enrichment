699d985d746717154b3ef26b0ec3d0f95f5c79d6
Nir Levy <nir@upsolver.com>
2024-09-10 17:46:34 +0300

    Add JWT decoding and arabic recognition

--- src/process/models/processManager.ts
+++ src/process/models/processManager.ts
@@ -2,20 +2,26 @@ import { Logger } from '@map-colonies/js-logger';
 import { inject, injectable } from 'tsyringe';
 import { SERVICES } from '../../common/constants';
 import { EnrichResponse, FeedbackResponse } from '../../common/interfaces';
+import jwt from 'jsonwebtoken';
+
+const arabicRegex = /[\u0600-\u06FF]/;
 
 @injectable()
 export class ProcessManager {
   public constructor(@inject(SERVICES.LOGGER) private readonly logger: Logger) {}
 
   public process(feedbackResponse: FeedbackResponse): EnrichResponse {
-    const selectedResponse = feedbackResponse.geocodingResponse.response.features[feedbackResponse.chosenResultId]
+    const selectedResponse = feedbackResponse.geocodingResponse.response.features[feedbackResponse.chosenResultId];
+    const token = jwt.decode(feedbackResponse.geocodingResponse.apiKey) as { system: string };
+    const { text } = feedbackResponse.geocodingResponse.response.geocoding.query;
+
     return {
       user: {
         name: feedbackResponse.geocodingResponse.userId,
       },
       query: {
-        language: '',
-        text: feedbackResponse.geocodingResponse.response.geocoding.query.text,
+        language: arabicRegex.test(text) ? 'ar' : 'he',
+        text: text,
       },
       result: {
         rank: feedbackResponse.chosenResultId,
@@ -24,10 +30,10 @@ export class ProcessManager {
         layer: selectedResponse.properties.layer,
         name: selectedResponse.properties.name.default,
       },
-      system: feedbackResponse.geocodingResponse.apiKey,
+      system: token?.system,
       site: feedbackResponse.geocodingResponse.site,
       // @ts-expect-error
       duration: new Date(feedbackResponse.responseTime) - new Date(feedbackResponse.geocodingResponse.respondedAt),
-    }
+    };
   }
 }
